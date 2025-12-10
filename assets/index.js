82dbdb7fd0908021ba0befdd1c8ac450d8331b8a
rfIhalas <rfranccizca@gmail.com>
2025-12-10 10:37:01 +0800

    Refactor theme toggle and add navigation functionality

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
-// Theme Toggle
+// SWITCH THEME
 document.addEventListener("DOMContentLoaded", () => {
     const toggleBtn = document.getElementById("toggle-theme");
     const body = document.body;
@@ -7,3 +7,33 @@ document.addEventListener("DOMContentLoaded", () => {
         body.classList.toggle("dark-theme");
     });
 });
+
+// NAV BARS
+document.addEventListener("DOMContentLoaded", function() {
+    const tabs = document.querySelectorAll(".nav-tabs button");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(btn => btn.classList.remove("active"));
+
+            tab.classList.add("active");
+
+            const target = document.getElementById(tab.dataset.target);
+            target.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
+
+// ACHIEVEMENTS
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.achievement h4, .project h4').forEach(title => {
+        title.addEventListener('click', () => {
+            const details = title.nextElementSibling;
+            if (details) details.classList.toggle('show');
+        });
+    });
+});
+
+
+
+
